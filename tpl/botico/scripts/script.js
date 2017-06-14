2760344e4b28fbb48beb3c6575eaff6623bc030e
Oleg Frolov <oleg_frolov@mail.ru>
2017-06-14 10:51:39 +0300

    ajax session kick

--- tpl/botico/scripts/script.js
+++ tpl/botico/scripts/script.js
@@ -2,13 +2,19 @@ $(document).ready(function(){
 	$("#aside .nav a").on("click",function(){
 		setcookie("mainmenu",$(this).parent().index());
 	});
-});
 
-$(document).ready(function(){
 	if (getcookie("mainmenu")!==undefined && getcookie("mainmenu")>0) {
 		var menu=getcookie("mainmenu")*1-1;
 		setTimeout(function(){
 			$("#aside .nav a:eq("+menu+")").focus().trigger("click");
 		},100);
 	}
+	ajax_sess_kick();
 });
+
+
+function ajax_sess_kick() {
+	setInterval(function(){
+		$.get("/engine/ajax.php?mode=ajax_sess_kick");
+	},120000);
+}
